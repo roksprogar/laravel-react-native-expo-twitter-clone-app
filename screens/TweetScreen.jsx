f62ad41595d50efb040e791dddc23548eb55a3c5
Rok Sprogar <rok.sprogar@gmail.com>
2022-06-26 09:56:29 +0200

    tweet screen layout and styles

--- screens/TweetScreen.jsx
+++ screens/TweetScreen.jsx
@@ -1,12 +1,129 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, StyleSheet, TouchableOpacity, Image, Text } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import { EvilIcons } from "@expo/vector-icons";
 
 const TweetScreen = () => {
-    return (
-        <View style={{ flex:1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>TweetScreen</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <TouchableOpacity style={styles.flexRow}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: "https://reactnative.dev/img/tiny_logo.png",
+            }}
+          />
+          <View>
+            <Text style={styles.tweetName}>Rok</Text>
+            <Text style={styles.tweetHandle}>@rok</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Entypo name="dots-three-vertical" size={24} color="gray" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tweetContentContainer}>
+        <Text style={styles.tweetContent}>
+          Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+          ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+        </Text>
+      </View>
+      <View style={styles.tweetEngagement}>
+        <View style={styles.flexRow}>
+          <Text style={styles.tweetEngagementNumber}>628</Text>
+          <Text style={styles.tweetEngagementLabel}>Retweets</Text>
         </View>
-    )
-}
+        <View style={[styles.flexRow, styles.ml4]}>
+          <Text style={styles.tweetEngagementNumber}>38</Text>
+          <Text style={styles.tweetEngagementLabel}>Quote Tweets</Text>
+        </View>
+        <View style={[styles.flexRow, styles.ml4]}>
+          <Text style={styles.tweetEngagementNumber}>2,128</Text>
+          <Text style={styles.tweetEngagementLabel}>Likes</Text>
+        </View>
+      </View>
+      <View style={[styles.tweetEngagement, styles.spaceAround]}>
+        <TouchableOpacity>
+          <EvilIcons name="comment" size={32} color="gray" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <EvilIcons name="retweet" size={32} color="gray" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <EvilIcons name="heart" size={32} color="gray" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <EvilIcons
+            name={Platform.OS === "ios" ? "share-apple" : "share-google"}
+            size={32}
+            color="gray"
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
-export default TweetScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  flexRow: {
+    flexDirection: "row",
+  },
+  ml4: {
+    marginLeft: 16,
+  },
+  spaceAround: {
+    justifyContent: 'space-around',
+  },
+  profileContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    marginRight: 8,
+    borderRadius: 25,
+  },
+  tweetName: {
+    fontWeight: "bold",
+    color: "#222222",
+  },
+  tweetHandle: {
+    color: "gray",
+    marginTop: 4,
+  },
+  tweetContentContainer: {
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e5e7eb",
+  },
+  tweetContent: {
+    fontSize: 20,
+    lineHeight: 30,
+  },
+  tweetEngagement: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e5e7eb",
+  },
+  tweetEngagementNumber: {
+    fontWeight: "bold",
+  },
+  tweetEngagementLabel: {
+    color: "gray",
+    marginLeft: 6,
+  },
+});
+
+export default TweetScreen;
